7cd6dc02c462087565511d0d88719006012384f7
Eriuden <antoinecarrette@hotmail.fr>
2024-03-01 09:58:13 +0100

    homePage

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='text-center'>
+        Découvrir et partager
+        <br className='max-md:hidden'/>
+        <span className='text-center'>Prompts générés par l'IA</span>
+      </h1>
+        
+    </section>
+  )
+}
