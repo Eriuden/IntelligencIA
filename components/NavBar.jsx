4c06648c9c7fe451cfeaca88adaa3abe0d3b0458
Eriuden <antoinecarrette@hotmail.fr>
2024-03-01 10:39:18 +0100

    navBar 1

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,11 +4,38 @@ import { useState, useEffect } from 'react'
 import {signIn, signOut, useSession, getProviders} from "next-auth/react"
 
 export const NavBar = () => {
+
+  const isUserLoggedIn = true
+
+  const [providers, setProviders] = useState(null)
+
+  useEffect(()=> {
+    const setTheProviders = async ()=> {
+        const response = await getProviders()
+
+        setTheProviders(response)
+    }
+
+    setProviders()
+  }, [])
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
         <Link href="/" className='flex gap-2 flex-center'>
             Acceuil
         </Link>
+
+        <div className='sm:flex hidden'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/create-prompt"> Créer un post</Link>
+                    <button type='button' onClick={signOut}>Déconnexion</button>
+
+                    <Link href="/profile">
+                        Profil 
+                    </Link>
+                </div>
+            ) :""}
+        </div>
     </nav>
   )
 }
