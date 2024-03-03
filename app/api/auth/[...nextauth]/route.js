838ab72f0539f3c6872ef6beb0d7ab2e84885e9b
Eriuden <antoinecarrette@hotmail.fr>
2024-03-03 13:59:50 +0100

    api Route 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 
+import User from "@/models/user";
 import { connectToDB } from "@/utils/DB";
 
 
@@ -12,12 +13,28 @@ const handler = NextAuth({
         })
     ],
     async session ({session}) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString()
+
+        return session 
     },
     async signIn({profile}) {
         try {
             await connectToDB()
-            
+
+            const userExists = await User.findOne({ email: profile.email})
+
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    userName: profile.name.replace("", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
             return true
         } catch (error) {
             console.log(error)
