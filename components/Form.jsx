2f8c7fe1ddc666f53878b5ddcdd7bea687ecf90d
Eriuden <antoinecarrette@hotmail.fr>
2024-03-05 08:50:03 +0100

    création prompt 3

--- components/Form.jsx
+++ components/Form.jsx
@@ -24,6 +24,34 @@ export const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
           required
           />
         </label>
+
+        <label>
+          <span className="text-gray-700">
+            Mots clés {` `}
+            <span>(#webDev, #nextJs)</span>
+          </span>
+
+          <input value={post.tag}
+          onChange={(e) => setPost({ ...post, tag :e.target.value})}
+          placeholder="#mots-clés"
+          required
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Annuler
+          </Link>
+
+          <button
+          type="submit"
+          disabled={submitting}
+          className="px-5 py-1.5 text-sm bg-primary-orange 
+          rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   )
