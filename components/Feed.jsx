a164e2258d2ca3c2bc8142cbbc8428342f01d528
Eriuden <antoinecarrette@hotmail.fr>
2024-03-08 08:28:29 +0100

    feed 1

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,51 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+import { PromptCard } from "./PromptCard"
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return(
+    <div className="mt-16">
+
+    </div>
+  )
+}
 
 export const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(()=> {
+    const fetchPosts = async () => {
+      const response = await fetch ("/api/prompt")
+      const data = await response.json()
+
+      setPosts(data)      
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section>
+      <form className="relative w-full flex-center">
+        <input 
+          type="text"
+          placeholder="Chercher un mot clé ou nom d'utilisateur"
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </form>
+
+      <PromptCardList 
+        data={{prompts}}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
