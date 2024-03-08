cc3acfa6f40b5b54305001fa6572a277e6bcbfde
Eriuden <antoinecarrette@hotmail.fr>
2024-03-08 08:47:53 +0100

    feed 4

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,6 +1,24 @@
+"use client"
 
-export const PromptCard = () => {
+import { useState } from "react"
+import Image from "next/image"
+import { useSession } from "next-auth/react"
+import { usePathname, useRouter } from "next/navigation"
+
+export const PromptCard = ({post, handleTagClick,
+handleEdit, handleDelete}) => {
   return (
-    <div>PromptCard</div>
+    <div>
+      < div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 
+        cursor-pointer">
+          <Image src={post.creator.image}
+            alt="Image_utilisateur"
+            width={40}
+            height={40}
+            className="rounded-full" />
+        </div> 
+      </div>
+    </div>
   )
 }
