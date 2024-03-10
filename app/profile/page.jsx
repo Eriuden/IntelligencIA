76f3983f270167bc9fc5dbf9328e07d3e3f5a7ec
Eriuden <antoinecarrette@hotmail.fr>
2024-03-10 21:27:28 +0100

    profil 1

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+"use client"
 
-export const Profile = () => {
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import { Profile } from "@/components/Profile"
+
+export const MyProfile = () => {
+
+  const {data: session} = useSession()
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=> {
+    const fetchPosts = async () => {
+      const response = await fetch (`/api/users/${session?.user.id
+      /posts}`)
+      const data = await response.json()
+
+      setPosts(data)   
+      
+      if (session?.user.id) fetchPosts()
+    }
+
+    fetchPosts()
+  }, [])
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async() => {
+
+  }
+
   return (
-    <div>Profil</div>
+    <Profile
+      name=""
+      desc=""
+      data={posts}
+      handleEdit={handleEdit()}
+      handleDelete={handleDelete()}
+    />
   )
 }
