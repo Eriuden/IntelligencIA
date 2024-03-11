ac337980f4fd7bca539c0616099a4d5a10681b39
Eriuden <antoinecarrette@hotmail.fr>
2024-03-11 19:57:02 +0100

    profil 3

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,6 +1,21 @@
+import { PromptCard } from "./PromptCard"
 
-export const Profile = () => {
+export const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
   return (
-    <div>Profile</div>
+    <section className="w-full">
+      <h1 className="text-left"> Profil de {name}</h1>
+      <p className="text-left">{desc}</p>
+
+      <div className="mt-10">
+        {data.map((post)=> {
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit (post)}
+            handleDelete={() => handleDelete && handleDelete (post)}
+          />
+        })}
+      </div>
+    </section>
   )
 }
