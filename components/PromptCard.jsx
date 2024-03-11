726212fad8beec41cd38c9c9ce0ad974080a0681
Eriuden <antoinecarrette@hotmail.fr>
2024-03-11 20:07:08 +0100

    profil 4

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,10 @@ import { usePathname, useRouter } from "next/navigation"
 export const PromptCard = ({post, handleTagClick,
 handleEdit, handleDelete}) => {
 
+  const {data:session} = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState("")
 
   const handleCopy = () => {
@@ -55,6 +59,20 @@ handleEdit, handleDelete}) => {
         onClick={() => handleTagClick(post.tag)}>
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id &&
+      pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t
+          border-gray-100 pt-3 ">
+          <p className="font-inter text-sm cursor-pointer"
+            onClick={handleEdit}
+          >Editer</p>
+
+          <p className="font-inter text-sm cursor-pointer"
+            onClick={handleDelete}
+          >Supprimer</p>
+        </div>
+      )}
     </div>
   )
 }
