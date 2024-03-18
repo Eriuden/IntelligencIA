2051ebe31bd51ddb979de8628d6d31689653a5e1
Eriuden <antoinecarrette@hotmail.fr>
2024-03-18 09:49:38 +0100

    prompt api 6

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -32,8 +32,21 @@ export const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`)
   }
 
-  const handleDelete = async() => {
-    
+  const handleDelete = async(post) => {
+    const hasConfirmed = confirm("Voulez vous supprimer cette prompt ?")
+
+    if(hasConfirmed) {
+      try {
+        await fetch(`api/prompt/${post._id.toString()}`, {method: "DELETE"})
+
+        const filteredPosts = posts.filter((p)=> 
+        p._id !== post._id)
+
+        setPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
